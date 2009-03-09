8876f2b314e2544bd912c424937566e013c48632
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2009-03-09 16:19:50 +0100

    Add setup script

--- csp/__init__.py
+++ csp/__init__.py
@@ -1,2 +1,5 @@
 
 from core import *
+
+__author__ = "Daniel Schierbeck"
+__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from distutils.core import setup
+
+import csp
+
+
+NAME = "python-csp"
+VERSION = csp.__version__
+AUTHOR = csp.__author__
+DESCRIPTION="Python implementation of Communicating Sequential Processes"
+
+
+setup(name=NAME,
+      description=DESCRIPTION,
+      version=VERSION,
+      author=AUTHOR,
+      packages=["csp"])
