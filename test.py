de4826f67a41de4889398b572b13d5cb5ef16392
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2009-03-09 12:07:09 +0100

    Add small test script

---
+++ test.py
@@ -0,0 +1,25 @@
+
+from csp import *
+
+
+@process
+def source(cin, cout):
+    cout << "foo" << "bar" << "baz"
+    poison(cout)
+
+
+@process
+def upcase(cin, cout):
+    for message in cin:
+        cout << message.upper()
+    poison(cout)
+
+
+@process
+def sink(cin, cout):
+    for message in cin:
+        print message
+
+
+p = pipeline(source(), upcase(), sink())
+parallel(p())
