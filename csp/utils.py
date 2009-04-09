4008d24f1f2ef265d66c5fdae3ca688dd027e175
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2009-04-09 20:19:08 +0200

    Remove unused utility functions

--- csp/utils.py
+++ csp/utils.py
@@ -1,5 +1,4 @@
 
-from itertools import ifilter
 from contextlib import contextmanager
 
 from csp.core import *
@@ -20,34 +19,6 @@ def spawned(*processes):
 
     for process in processes:
         sync(process)
-
-
-def pool(processes):
-    @process
-    def _pool(cin, cout):
-        parallel(*[p(cin=cin, cout=cout) for p in processes])
-        poison(cout)
-
-    return _pool
-
-def map(func):
-    @process
-    def _map(cin, cout):
-        for message in cin:
-            cout << func(message)
-        poison(cout)
-
-    return _map()
-
-
-def filter(predicate=None):
-    @process
-    def _filter(cin, cout):
-        for message in ifilter(predicate, cin):
-            cout << message
-        poison(cout)
-
-    return _filter()
 
 
 @process
